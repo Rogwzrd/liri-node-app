e9df53ddf2fefde294a69a02a98c89f8b77d3c71
unknown <mgdaye@gmail.com>
2017-10-11 19:20:42 -0400

    got tweets to return

--- liri.js
+++ liri.js
@@ -1,20 +1,56 @@
 //initail input variables for node.js
 var cmd = process.argv[2],
-	input = process.argv[3];
+    input = process.argv[3],
+    keys = require("./keys.js");
 
-switch(cmd) {
-	case "movie-this":
-	console.log("omdb api");
-	break;
-	case "spotify-this-song":
-	console.log("spotify api");
-	break;
-	case "my-tweets":
-	console.log("twitter api");
-	break;
-	case "do-what-it-says":
-	console.log("text file call");
-	default:
-	console.log("you need to try a different command to get run this file");
+switch (cmd) {
+    case "movie-this":
+
+        console.log("omdb api");
+
+        break;
+
+    case "spotify-this-song":
+
+        console.log("spotify api");
+
+        break;
+
+    case "my-tweets":
+        var Twitter = require('twitter');
+
+        var params = { q: 'node.js' };
+
+        var client = new Twitter(keys.twitterKeys)
+
+        client.get('search/tweets', params, function(error, tweets, response) {
+
+        	if (error){
+
+        		return console.log("this is an error:" + JSON.stringify(error));
+        	};
+            if (tweets) {
+            	for (var x = 0; x < tweets.statuses.length; x++){
+            	console.log(`#${x} this tweet was created on: ${tweets.statuses[x].created_at}`);
+            	console.log(`#${x} tweet: ${tweets.statuses[x].text}`);
+            	}
+                // console.log(tweets);
+            };
+            // if (response) {
+
+            // 	console.log("this is the response: " + response);
+            // }
+
+        });
+
+        break;
+
+    case "do-what-it-says":
+
+        console.log("text file call");
+
+    default:
+
+        console.log("you need to try a different command to run this file");
 
 }
