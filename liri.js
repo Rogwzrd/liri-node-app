f5996e6fd2d21034ab50cdbd71cb0efc94d9ea64
unknown <mgdaye@gmail.com>
2017-10-16 19:24:33 -0400

    added do-what-it-says functionality

--- liri.js
+++ liri.js
@@ -3,121 +3,145 @@ var cmd = process.argv[2],
     input = process.argv,
     keys = require("./keys.js");
 
-switch (cmd) {
-    case "movie-this":
-        var request = require("request");
+function liriSwitch(userCommand, userInput) {
+    switch (userCommand) {
 
-        var query = [];
+        case "movie-this":
+            var request = require("request");
 
-        for (var i = 3; i < input.length; i++) {
+            var query = [];
 
-            var word = input[i];
+            for (var i = 3; i < userInput.length; i++) {
 
-            query.push(word);
-        }
-        query = query.join("+");
+                var word = input[i];
 
-
-        request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-
-            // If there were no errors and the response code was 200 (i.e. the request was successful)...
-            if (!error && response.statusCode === 200) {
-
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year of release: " + JSON.parse(body).Year);
-                console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1]["Value"]);
-                console.log("Country of Origin: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
+                query.push(word);
             }
-        });
+            query = query.join("+");
 
-        break;
 
-    case "spotify-this-song":
+            request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece", function (error, response, body) {
 
-        var spotifyApi = require('node-spotify-api');
+                if (error) {
+                    console.log(error);
+                }
 
-        var spotify = new spotifyApi(keys.spotifyKeys);
+                if (!error && response.statusCode === 200) {
 
-        var query = [];
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Year of release: " + JSON.parse(body).Year);
+                    console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+                    console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1]["Value"]);
+                    console.log("Country of Origin: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                }
+            });
 
-        for (var i = 3; i < input.length; i++) {
+            break;
 
-            var word = input[i];
+        case "spotify-this-song":
 
-            query.push(word);
-        }
+            var spotifyApi = require('node-spotify-api');
 
-        query = query.join(" ");
+            var spotify = new spotifyApi(keys.spotifyKeys);
 
-        spotify.search({ type: 'track', query: query, limit: 20 }, function(err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
+            var query = [];
 
-            if (data) {
-                artistArray = [];
-                for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
-                    let artist = (data.tracks.items[0].artists[i].name);
-                    artistArray.push(artist);
-                    console.log("Artist(s): " + artistArray)
-                };
-                
-                console.log("Song title: " + data.tracks.items[0].name);
-                console.log("Preview: " + data.tracks.items[0].preview_url);
-                console.log("Album title: " + data.tracks.items[0].album.name);
-            }
-        });
+            if (typeof userInput === "object") {
 
-        break;
+                for (var i = 3; i < userInput.length; i++) {
 
-    case "my-tweets":
-        var Twitter = require('twitter');
+                    var word = input[i];
 
-        var params = { q: "mikeD_Developer", count: 20 };
+                    query.push(word);
 
-        var client = new Twitter(keys.twitterKeys)
+                    query = query.join(" ");
+                }
+            } else {
 
-        client.get('search/tweets', params, function(error, tweets, response) {
-
-            if (error) {
-                return console.log("this is an error:" + JSON.stringify(error));
-            };
-
-            if (tweets) {
-                for (var x = 0; x < tweets.statuses.length; x++) {
-                    console.log(`#${x + 1} this tweet was created on: ${tweets.statuses[x].created_at}`);
-                    console.log(`#${x + 1} tweet: ${tweets.statuses[x].text}`);
-                };
-            };
-        });
-
-        break;
-
-    case "do-what-it-says":
-
-        var fs = require("fs");
-
-        fs.readFile("random.txt", "utf8", function(err, data) {
-
-            if (err) {
-                return err;
+                query = userInput;
             }
 
-            if (data) {
-                console.log(data)
-            };
-        });
+            spotify.search({type: 'track', query: query, limit: 20}, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
 
-        console.log("text file call");
+                if (data) {
+                    artistArray = [];
+                    for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+                        let artist = (data.tracks.items[0].artists[i].name);
+                        artistArray.push(artist);
+                        console.log("Artist(s): " + artistArray)
+                    }
+                    ;
 
-        break;
+                    console.log("Song title: " + data.tracks.items[0].name);
+                    console.log("Preview: " + data.tracks.items[0].preview_url);
+                    console.log("Album title: " + data.tracks.items[0].album.name);
+                }
+            });
 
-    default:
+            break;
 
-        console.log("you need to try a different command to run this file");
+        case "my-tweets":
+            var Twitter = require('twitter');
 
+            var params = {q: "mikeD_Developer", count: 20};
+
+            var client = new Twitter(keys.twitterKeys)
+
+            client.get('search/tweets', params, function (error, tweets, response) {
+
+                if (error) {
+                    return console.log("this is an error:" + JSON.stringify(error));
+                }
+                ;
+
+                if (tweets) {
+                    for (var x = 0; x < tweets.statuses.length; x++) {
+                        console.log(`#${x + 1} this tweet was created on: ${tweets.statuses[x].created_at}`);
+                        console.log(`#${x + 1} tweet: ${tweets.statuses[x].text}`);
+                    }
+                    ;
+                }
+                ;
+            });
+
+            break;
+
+        case "do-what-it-says":
+
+            var fs = require("fs");
+
+            fs.readFile("random.txt", "utf8", function (err, data) {
+
+                if (err) {
+                    return err;
+                }
+
+                if (data) {
+
+                    var info = data.split(',');
+                    console.log(info);
+                    console.log(info[1].replace(/"/g, ""))
+                    liriSwitch(info[0], info[1].replace(/"/g, ""));
+
+                }
+                ;
+            });
+
+            console.log("text file call");
+
+            break;
+
+        default:
+
+            console.log("you need to try a different command to run this file");
+
+    }
 }
+
+liriSwitch(cmd,input);
